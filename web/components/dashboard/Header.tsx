b32323ff4ba6bdfa414809e4c9949fa779f62d81
Brook Worku <brookwworku3@gmail.com>
2024-04-20 00:29:43 +0300

    Implemented dashboard Ui and Shelf listing UI

---
+++ web/components/dashboard/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <section className="bg-gray-200 ml-24">
+      <div className="flex justify-between pt-4 px-4">
+        <h2 className="p-4 font-serif font-semibold">DashBoard</h2>
+        <h2 className="p-4 font-serif font-semibold">Welcome Back</h2>
+      </div>
+
+      <div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default Header;
